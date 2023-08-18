1bf96d34c3bc1c816e11e3c25c49a328fe9e1a94
Denis vasquez <clasic.exper2023@gmail.com>
2023-08-17 22:24:02 -0600

    first commit

---
+++ desendente.py
@@ -0,0 +1,8 @@
+
+
+numero = int(input("ingrese el valor de n: "))
+i = numero
+for i in range (numero, 0, -1):
+    print(i)
+
+
---
+++ mttokm.py
@@ -0,0 +1,4 @@
+metros = int(input("ingresa la cantidad de metros a convertir: "))
+resultado = (metros * 1) / 1000
+
+print(metros, "metros convertidos a kilometros es: ",resultado, "kilometros")
